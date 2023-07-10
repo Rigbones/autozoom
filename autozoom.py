ef6e98c0653439e53c484c249121a4361b49831b
Rigbones <94621145+Rigbones@users.noreply.github.com>
2023-07-10 11:42:14 +0800

    fixed lots of stuff

--- autozoom.py
+++ autozoom.py
@@ -4,10 +4,16 @@ import datetime as dt
 import time
 import click
 
+# path to google chrome exe file
+# CHROME_PATH = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+# wb.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
+# wb.register("chrome", )
+# today
+TODAY = dt.datetime.today()
 # ISC 311 zoom link
 ZOOM_LINK = r"https://korea-ac-kr.zoom.us/j/97797472566?pwd=NjI5ZENFV2hDU1BPVWpMd21kNXNzQT09"
-# Meeting time
-MEETING_TIME = dt.time(hour=8, minute=0, second=0)
+# Meeting datetime
+MEETING_DATETIME = dt.datetime(year=TODAY.year, month=TODAY.month, day=TODAY.day, hour=8, minute=0, second=0)
 
 def format_date(x):
     date_list = x.split(sep="-")
@@ -22,41 +28,41 @@ def given_datetime(given_date, given_time):
     # YY, MM, DD, HH, MM
     return dt.datetime(given_date[2], given_date[1], given_date[0], given_time[0], given_time[1], given_time[2])
 
-def join_meeting(zoom_link, meeting_date, meeting_time):
+def join_meeting(zoom_link: str, meeting_datetime: dt.datetime):
     """
     Joins zoom meeting automatically at the specified time
     Args:
         zoom_link: raw string.  The meeting link
-        meeting_time: time object.  The meeting time
+        meeting_datetime: python datetime object.  The meeting datetime
     """
+    # detect time now and calculate time left until class starts
+    while (True):
+        now = dt.datetime.now()
+        seconds_left_until_class = (meeting_datetime - now).total_seconds()
+        # if class is due to start
+        if (seconds_left_until_class < 0):
+            print("Class starting!  Joining zoom...")
+            break
+        # if class hasn't started yet
+        print(f"{seconds_left_until_class} seconds left until class starts.")
+        dt.time.sleep(30)  # sleeps for 30 seconds
+    # join meeting
+    wb.open(url=zoom_link, new=2)
+    
 
-    now = dt.datetime.now()
+join_meeting(ZOOM_LINK, MEETING_DATETIME)
 
-    # time difference between current and meeting time
-    wait_time_sec = (required_datetime - dt.datetime.now().replace(microsecond=0)).total_seconds()
-    print("Your ZOOM meeting starts in " + str(wait_time_sec/60) + " min")
-    time.sleep(wait_time_sec)
+# now = dt.datetime.now()
+# start = dt.datetime(2023, now.month, now.day, 8, 0, 0)
+# time_delta = now - now
 
-    # zoom app related
-    wb.get(using='chrome').open(zoom_link, new=2) #open zoom link in a new window
-    time.sleep(5) # given time for the link to show app top-up window
-    pyg.click(x=805, y=254, clicks=1, interval=0, button='left') # click on open zoom.app option
-    time.sleep(10) # wait for 10 sec
-    pyg.click(x=195, y=31, clicks=1, interval=0, button='left') # maximize zoom app
-    time.sleep(3) # wait for 3 sec
-    pyg.click(x=50, y=776, clicks=1, interval=0, button='left')
-
-now = dt.datetime.now()
-start = dt.datetime(2023, now.month, now.day, 8, 0, 0)
-time_delta = now - now
-
-# while not 09:40 yet
-while (time_delta.total_seconds() < 6000):
-    # press enter every 5 seconds
-    pyg.press("enter", presses=1, interval=5)
-    print("ENTER key pressed at", now)
-    # calculate new time difference between 08:00 and now
-    now = dt.datetime.now()
-    time_delta = now-start
+# # while not 09:40 yet
+# while (time_delta.total_seconds() < 6000):
+#     # press enter every 5 seconds
+#     pyg.press("enter", presses=1, interval=5)
+#     print("ENTER key pressed at", now)
+#     # calculate new time difference between 08:00 and now
+#     now = dt.datetime.now()
+#     time_delta = now-start
 
     
